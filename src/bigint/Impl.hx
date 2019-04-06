788e88cae501b44b2701c31511ee03b89c66b829
Kevin Leung <kevinresol@gmail.com>
2019-04-06 09:21:40 +0800

    Add some reference links [ci skip]

--- src/bigint/Impl.hx
+++ src/bigint/Impl.hx
@@ -2,4 +2,8 @@ package bigint;
 
 class Impl {
 	// TODO: Pure Haxe implementation here
+	
+	// ref: https://github.com/soywiz/haxe-crypto/blob/master/src/com/hurlant/math/BigInteger.hx
+	// ref: https://github.com/fponticelli/thx.core/blob/master/src/thx/BigInt.hx
+	
 }
